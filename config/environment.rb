19bb7e37baa93ff4060351e27c02ed15aa0f0941
Pierr <pierre.besson7@gmail.com>
2011-11-20 23:24:49 +0100

    initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+IKine2::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IKine2::Application.config.secret_token = 'b33ec60a1317988556f12016b41589beec5691021cca3f533799544dd9e9e9124c25a8fd01c0ff6179ad1c43fd4520091dad25e88fe2f1ddbb920c832c183304'
