7d36d4b12fca74d18df494fd5830449ce7bac239
Pierr <pierre.besson7@gmail.com>
2011-12-04 15:19:18 +0100

    Small doc maj

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,15 @@
+#Class which defines the home page.
 class PagesController < ApplicationController
+  #Conroller for Home page.
   def home
     @title = "Home"
   end
-
+  #Controller for the Contact page
   def contact
     @title = "Contact"
   end
-  
+  #Conttoller for the about page
   def about
     @title = "About"
   end
-
 end
